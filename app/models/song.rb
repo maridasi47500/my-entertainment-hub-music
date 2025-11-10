0ccac0a0050507cddc6c24980ae46925fd95ce1e
Anonni Mousse <61146153+maridasi47500@users.noreply.github.com>
2025-11-09 22:51:55 -0300

    q<srgdqerg

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,5 +8,20 @@ end
 def mysong
 read_attribute(:mysong)
 end
+def self.track_list
+xx=select("songs.*").to_a.shuffle.map do |x|
+  {
+    id: x.id,
+    name: x.title,
+    artist: x.artist,
+      myradio: "myradio",
+    image: ("https://source.unsplash.com/Qrspubmx6kE/640x360"),
+    path: "/uploads/"+x.mysong+""
+  }
+end
+xx.to_json
+end
+
+
 
 end
--- public/myplayer.js
+++ public/myplayer.js
@@ -1,26 +1,30 @@
 // Select all the elements in the HTML page
+
+function initme(myid){
+console.log(myid);
 // and assign them to a variable
-let now_playing = document.querySelector(".now-playing");
-let track_art = document.querySelector(".track-art");
-let track_id = document.querySelector(".track-id");
-let track_name = document.querySelector(".track-name");
-let track_artist = document.querySelector(".track-artist");
+let now_playing = document.querySelector(".now-playing"+myid);
+let track_art = document.querySelector(".track-art"+myid);
+let track_id = document.querySelector(".track-id"+myid);
+let track_name = document.querySelector(".track-name"+myid);
+let track_artist = document.querySelector(".track-artist"+myid);
  
-let playpause_btn = document.querySelector(".playpause-track");
-let next_btn = document.querySelector(".next-track");
-let prev_btn = document.querySelector(".prev-track");
+let playpause_btn = document.querySelector(".playpause-track"+myid);
+let next_btn = document.querySelector(".next-track"+myid);
+let prev_btn = document.querySelector(".prev-track"+myid);
  
-let seek_slider = document.querySelector(".seek_slider");
-let volume_slider = document.querySelector(".volume_slider");
-let curr_time = document.querySelector(".current-time");
+let seek_slider = document.querySelector(".seek_slider"+myid);
+let volume_slider = document.querySelector(".volume_slider"+myid);
+let curr_time = document.querySelector(".current-time"+myid);
 let myhash;
 try{
-myhash = JSON.parse(document.querySelector("#lyrichash").innerHTML);
+myhash = JSON.parse(document.querySelector(".lyrichash"+myid).innerHTML);
 }
 catch(e){
 myhash={};
 }
-let total_duration = document.querySelector(".total-duration");
+
+let total_duration = document.querySelector(".total-duration"+myid);
 // Specify globally used values
 let track_index = 0;
 let isPlaying = false;
@@ -28,9 +32,10 @@ let updateTimer;
  
 // Create the audio element for the player
 let curr_track = document.createElement('audio'); 
-let my_curr_track = document.querySelector('#mycurrtrack'); 
+let mycurrtrack = document.querySelector('.mycurrtrack'+myid); 
 // Define the list of tracks that have to be played
-let track_list = JSON.parse(document.querySelector("#track_list").innerHTML);
+let track_list = JSON.parse(document.querySelector(".track_list"+myid).innerHTML);
+
 
 
 function loadTrack(track_index) {
@@ -41,7 +46,7 @@ function loadTrack(track_index) {
   // Load a new track
   console.log(track_list.length, track_index);
   curr_track.src = track_list[track_index].path;
-  document.querySelector('#mycurrtrack').innerHTML=track_list[track_index].path; 
+  mycurrtrack.innerHTML=track_list[track_index].path; 
 // Define the list of tracks that have to be played
   curr_track.load();
  
@@ -65,7 +70,7 @@ function loadTrack(track_index) {
   curr_track.addEventListener("error", nextTrackError);
  
   // Apply a random background color
-  random_bg_color();
+  //random_bg_color();
 }
  
 function random_bg_color() {
@@ -80,7 +85,7 @@ function random_bg_color() {
  
   // Set the background to the new color
   //document.body.style.background = bgColor;
-  document.body.style.background = "black";
+  //document.body.style.background = "black";
 }
  
 // Function to reset all values to their default
@@ -92,18 +97,13 @@ function resetValues() {
 
 
 
-function playpauseTrack() {
-  // Switch between playing and pausing
-  // depending on the current state
-  if (!isPlaying) playTrack();
-  else pauseTrack();
-}
+
  
 function playTrack() {
   // Play the loaded track
   curr_track.play();
   isPlaying = true;
-  playpause_btn.innerHTML = '&#x2016;';
+  playpause_btn.innerHTML = '&#x23f8;';
 
 
  
@@ -121,12 +121,20 @@ function pauseTrack() {
 
 }
  
+function playpauseTrack() {
+  // Switch between playing and pausing
+  // depending on the current state
+  if (!isPlaying) playTrack();
+  else pauseTrack();
+}
+playpause_btn.addEventListener("click", playpauseTrack); 
 function nextTrackError(e,err) {
   console.log(JSON.stringify(e)) // Go back to the first track if the
   console.log((e,err)) // Go back to the first track if the
   if (typeof JSON.parse(JSON.stringify(e))["isTrusted"] !== "undefined"){
   // current one is the last in the track list
 	  track_list.splice(track_index, 1);
+
 	 if (window.next){
   if (track_index >= track_list.length){
     track_index =+ 1;
@@ -156,6 +164,7 @@ function nextTrack() {
   loadTrack(track_index);
   playTrack();
 }
+next_btn.addEventListener("click", nextTrack); 
  
 function idontlike() {
 $.ajax({url:"/songs/"+track_id.innerHTML+"/idontlike"});
@@ -172,6 +181,7 @@ function prevTrack() {
   loadTrack(track_index);
   playTrack();
 }
+prev_btn.addEventListener("click", prevTrack); 
 
 
 
@@ -184,12 +194,14 @@ function seekTo() {
   // Set the current track position to the calculated seek position
   curr_track.currentTime = seekto;
 }
+ seek_slider.addEventListener("change", seekTo); 
  
 function setVolume() {
   // Set the volume according to the
   // percentage of the volume slider set
   curr_track.volume = volume_slider.value / 100;
 }
+ volume_slider.addEventListener("change", setVolume); 
  
 function seekUpdate() {
   let seekPosition = 0;
@@ -215,10 +227,7 @@ function seekUpdate() {
     // Display the updated duration
     curr_time.textContent = currentMinutes + ":" + currentSeconds;
 
-    curr_time_m.textContent = currentMinutes + ":" + currentSeconds+"."+currentmilliSeconds;
-    if (myhash[document.querySelector('#mycurrtrack').innerHTML][(currentMinutes + ":" + currentSeconds+"."+currentmilliSeconds)]){
-      displaylyric.innerHTML=myhash[document.querySelector('#mycurrtrack').innerHTML][(currentMinutes + ":" + currentSeconds+"."+currentmilliSeconds)];
-     };
+    document.querySelector('.curr_time_m'+myid).textContent = currentMinutes + ":" + currentSeconds+"."+currentmilliSeconds;
     total_duration.textContent = durationMinutes + ":" + durationSeconds;
     
 
@@ -226,6 +235,21 @@ function seekUpdate() {
 }
 
 
-
-// Load the first track in the tracklist
 loadTrack(track_index);
+return false;
+};
+
+
+$(function(){
+// Load the first track in the tracklist
+$("[data-music-release-id]").each(function(){
+var myid=String($(this)[0].dataset.musicReleaseId);
+try{
+initme(myid);
+}
+catch(e){
+console.log("error");
+}
+});
+
+});
